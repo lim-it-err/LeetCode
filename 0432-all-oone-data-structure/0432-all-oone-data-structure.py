3c7dce2e9f50368aefb22b2cf217f30ce940e038
JeongHanLim <40853979+lim-it-err@users.noreply.github.com>
2024-02-08 21:11:25 +0900

    Time: 185 ms (32.95%), Space: 32.2 MB (94.51%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.py
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+
+
 class AllOne:
     def __init__(self):
         self.value_frequency = {}
@@ -11,19 +13,18 @@ class AllOne:
         del self.frequency_value[cur_freq][key]
         if not self.frequency_value[cur_freq]:
             del self.frequency_value[cur_freq]
-        self.frequency_value[cur_freq+1][key] = 1
-        self.value_frequency[key]+=1
-
+        self.frequency_value[cur_freq + 1][key] = 1
+        self.value_frequency[key] += 1
 
     def dec(self, key: str) -> None:
         assert key in self.value_frequency, "No key in dec operation"
         cur_freq = self.value_frequency[key]
         del self.frequency_value[cur_freq][key]
-        if cur_freq-1>0:
-            self.frequency_value[cur_freq-1][key] = 1
+        if cur_freq - 1 > 0:
+            self.frequency_value[cur_freq - 1][key] = 1
         if not self.frequency_value[cur_freq]:
             del self.frequency_value[cur_freq]
-        self.value_frequency[key]-=1
+        self.value_frequency[key] -= 1
         if self.value_frequency[key] == 0:
             del self.value_frequency[key]
         print(self.value_frequency)
@@ -41,10 +42,11 @@ class AllOne:
 
         min_frequent = min(self.frequency_value.keys())
         return next(iter(self.frequency_value[min_frequent]))
-            
+
     def _add_new(self, key):
         self.value_frequency[key] = 0
         self.frequency_value[0][key] = 0
+
 
 # Your AllOne object will be instantiated and called as such:
 # obj = AllOne()
@@ -52,6 +54,3 @@ class AllOne:
 # obj.dec(key)
 # param_3 = obj.getMaxKey()
 # param_4 = obj.getMinKey()
-
-
-1 1 1 2 2 2 3 3 4 4 4 5 5 5 6 -> 1 1 1 3
